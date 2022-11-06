093ab8510e13d359a86378ef6cf00dfeb52a93a1
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-06 00:23:59 -0400

    complete food_js exercise where i can display an out by onchange event from dropdown list

---
+++ food_mart.js
@@ -0,0 +1,42 @@
+let menu = {
+   drinks : [
+      "Water", "Tea", "Sweet Tea",
+      "Coke", "Dr. Pepper", "Sprite"
+   ],
+   entrees : [
+      "Hamburger w/ Fries",
+      "Grilled Cheese w/ Tater Tots",
+      "Grilled Chicken w/ Veggies",
+      "Chicken Fried Steak w/ Mashed Potatoes",
+      "Fried Shrimp w/ Coleslaw",
+      "Veggie Plate"
+   ],
+   desserts: [
+      "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+   ]
+};
+
+let foodMenu = document.getElementById('foodMenu');
+
+let objectArray = [];
+let keyArray = Object.keys(menu);
+
+for (items in menu) {
+   let theOption = new Option(items);
+   foodMenu.appendChild(theOption); 
+   objectArray.push(menu[items]);
+}
+
+foodMenu.onchange = getItems;
+
+function getItems() {
+   for (let i = 0; i < keyArray.length; i++) {
+      if (foodMenu.value == keyArray[i]) {
+         console.log(`You have options to choose: ${objectArray[i]}`);
+      }
+   }
+}
+
+
+
+
